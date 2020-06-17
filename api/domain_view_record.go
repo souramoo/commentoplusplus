894c3e85a979e90068b28256cc0375f73f4aef0c
Souradip Mookerjee <sm2030@cam.ac.uk>
2020-06-17 23:09:08 +0100

    Update domain_view_record.go

--- api/domain_view_record.go
+++ api/domain_view_record.go
@@ -5,11 +5,11 @@ package main
 //)
 
 func domainViewRecord(domain string, commenterHex string) {
-	statement := `
-		INSERT INTO
-		views  (domain, commenterHex, viewDate)
-		VALUES ($1,     $2,           $3      );
-	`
+	// statement := `
+	//	INSERT INTO
+	//	views  (domain, commenterHex, viewDate)
+	//	VALUES ($1,     $2,           $3      );
+	//`
 	// _, err := db.Exec(statement, domain, commenterHex, time.Now().UTC())
 	// if err != nil {
 	// 	logger.Warningf("cannot insert views: %v", err)
