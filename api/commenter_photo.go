9c083de2f6b02adb5f6c9a1aef63cd8aa8671b3f
Souradip Mookerjee <souramoo@gmx.com>
2020-11-02 00:38:31 +0000

    Fix google photo bug

--- api/commenter_photo.go
+++ api/commenter_photo.go
@@ -21,6 +21,8 @@ func commenterPhotoHandler(w http.ResponseWriter, r *http.Request) {
 	if c.Provider == "google" {
 		if strings.HasSuffix(url, "photo.jpg") {
 			url += "?sz=38"
+		} else if strings.HasSuffix(url, "=s96-c") {
+			url = url[:len(url) - len("=s96-c")] + "=s38"
 		} else {
 			url += "=s38"
 		}
