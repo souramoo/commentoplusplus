bf82e6733072f8940c6b3d91fe296c7d796567c0
Souradip Mookerjee <souramoo@gmx.com>
2021-05-23 21:58:09 +0100

    Serve /i18n

--- api/router_static.go
+++ api/router_static.go
@@ -76,7 +76,7 @@ func staticRouterInit(router *mux.Router) error {
 		return err
 	}
 
-	for _, dir := range []string{"/js", "/css", "/images", "/fonts"} {
+	for _, dir := range []string{"/js", "/css", "/images", "/fonts", "/i18n"} {
 		files, err := ioutil.ReadDir(os.Getenv("STATIC") + dir)
 		if err != nil {
 			logger.Errorf("cannot read directory %s%s: %v", os.Getenv("STATIC"), dir, err)
