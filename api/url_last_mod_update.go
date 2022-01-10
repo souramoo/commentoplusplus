a859b5e7963a1e0acffd3309550ce12c0bab9914
nikhilreddy <nikhil.reddy@getmega.com>
2022-01-10 11:21:13 +0530

    fix json request

--- api/url_last_mod_update.go
+++ api/url_last_mod_update.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bytes"
-	"fmt"
+	"encoding/json"
 	"net/http"
 	"strings"
 	"time"
@@ -17,12 +17,44 @@ func updateUrlLastModTime(pageUrl string) error {
 		url = ProdEndPoint
 	}
 
-	jsonStr := fmt.Sprintf("{\"url_entries\": {\"url\": %s, \"last_mod\": %s)}}", path, time.Now().Format(time.RFC3339))
+	body := getPayloadBody(path, time.Now().Format(time.RFC3339))
 
-	_, err := http.Post(url, "application/json", bytes.NewBuffer([]byte(jsonStr)))
+	_, err := http.Post(url, "application/json", bytes.NewBuffer(body))
 
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+type (
+	UrlEntry struct {
+		URL     string `json:"url"`
+		LastMod string `json:"last_mod"`
+	}
+	Request struct {
+		UrlEntries []UrlEntry `json:"url_entries"`
+	}
+)
+
+func getPayloadBody(path string, time string) []byte {
+	var entries []UrlEntry
+
+	entry := UrlEntry{
+		URL:     path,
+		LastMod: time,
+	}
+
+	entries = append(entries, entry)
+
+	data := &Request{
+		UrlEntries: entries,
+	}
+
+	dataBytes, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+
+	return dataBytes
 }
