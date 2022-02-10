b5c95ebbebdc39e02a3314379eb5cdc7831c7bb6
Sarvesh Raj <sarvesh0803@gmail.com>
2022-02-10 12:02:59 +0530

    Update Omega URL

--- api/constants.go
+++ api/constants.go
@@ -4,7 +4,7 @@ var version string
 
 // omega service endpoints to update last mods for pages
 
-const StagingEndPoint string = "https://omega-dot-getmega-app.appspot.com/twirp/omega.pb.Website/UpdateUrlLastModTime"
+const StagingEndPoint string = "https://staging-gw.getmega.co/omega/twirp/omega.pb.Website/UpdateUrlLastModTime"
 const ProdEndPoint string = "https://omega-dot-mega-prod-ceef8.appspot.com/twirp/omega.pb.Website/UpdateUrlLastModTime"
 
 const CommentoProdDomain string = "www.getmega.com"
