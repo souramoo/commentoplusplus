63d7a84f04d8fbbf971ccf29ea3d0eed67c5b2ac
Souradip Mookerjee <souramoo@gmx.com>
2022-04-04 09:10:12 +0100

    Update version.go

--- api/version.go
+++ api/version.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"bytes"
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"time"
-)
-
 func versionPrint() error {
 	logger.Infof("starting Commento %s", version)
 	return nil
